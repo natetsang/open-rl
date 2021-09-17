f9f268d0ef640ea6608351e6695f41d71b11c64a
Nate Tsang <nate.k.tsang@gmail.com>
2021-09-16 20:47:08 -0700

    update BaseAgent interface

--- openrl/agents/base_agent.py
+++ openrl/agents/base_agent.py
@@ -1,17 +1,18 @@
 import tensorflow as tf
+from typing import Tuple, Union
 
 
 class BaseAgent:
-    def test_agent(self):
+    def run_episode(self):
         raise NotImplementedError
 
-    def run_episode(self):
+    def run_agent(self):
         raise NotImplementedError
 
     def save_models(self) -> None:
         raise NotImplementedError
 
-    def load_models(self) -> tf.keras.Model:
+    def load_models(self) -> Union[tf.keras.Model, Tuple[tf.keras.Model]]:
         raise NotImplementedError
 
 
