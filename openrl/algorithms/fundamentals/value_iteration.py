f9620d5214fcd5bc4dedbfa66b80d0da7e8479ab
Nate T <39641125+natetsang@users.noreply.github.com>
2022-12-27 13:23:29 -0800

    create value_iteration.py

---
+++ openrl/algorithms/fundamentals/value_iteration.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+
+def value_iteration(P: dict, gamma: float, theta: float) -> tuple[np.ndarray, dict[int, int]]:
+    num_states = len(P)
+    num_actions = len(P[0])
+    policy = {}
+    delta = 0
+
+    # Initialize V(s), for all s in S, arbitrarily except that V(terminal) = 0
+    V = np.zeros(num_states)
+
+    # Run multiple sweeps through state space until convergence
+    while delta < theta:
+        Q = np.zeros((num_states, num_actions))  # Initialize q-values for every sweep
+
+        # loop through each state s in S
+        for s in range(num_states):
+            # Get all q-values
+            for a in range(num_actions):
+                for prob, next_state, reward, done in P[s][a]:
+                    Q[s][a] += prob * (reward + gamma * V[next_state] * (not done))
+
+            # Extract the greedy policy w.r.t. the q-values
+            policy[s] = np.argmax(Q[s])
+
+        # calculate delta
+        V_new = np.max(Q, axis=1)
+        delta = np.max(np.abs(V - V_new))
+
+        # update V
+        V = V_new.copy()
+
+    return V, policy
